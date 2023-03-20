0f7689306ecb8e7264a44d7b9fc65c58027d57e8
samroodAli <devsamrood@gmail.com>
2023-03-20 19:12:48 +0530

    feat: add cors, morgan and some other middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import express from "express";
 import router from "./router";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", router);
 
 app.get("/status", (_, res) => {
   res.status(200);
-  res.json({ message: "app is running" });
+  res.json({ message: "app design in node v4 is running" });
 });
 
 export default app;
